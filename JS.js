58130ce466e11927055a602ec8b649105777b14f
arh91 <alvaro_rh_991@hotmail.com>
2023-09-16 17:41:01 +0200

    modificamos el código para que el programa muestre únicamente la última consulta realizada, y para notificar al usuario en caso de que no haya conexión a Internet

--- JS.js
+++ JS.js
@@ -4,44 +4,62 @@ const select = document.querySelector(".top-banner select");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
 const apiKey = "89b9fdb6f95944991fd97b6727d65c78";
+let li = document.createElement("li");
+//const conex = "false"
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const listItems = list.querySelectorAll(".ajax-section .city");
-  const inputVal = select.value;
+/* if(navigator.onLine) {
+  conex = "true";
+}else {
+  alert("No se pueden obtener los datos sin conexión a Internet. Conéctese a la Red e inténtelo de nuevo");
+} */
 
-  //AJAX
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const { main, name, sys, weather } = data;
-      const icon = `https://openweathermap.org/img/wn/${
-        weather[0]["icon"]
-      }@2x.png`;
-      const li = document.createElement("li");
-      li.classList.add("city");
-      const markup = `
-        <h2 class="city-name" data-name="${name},${sys.country}">
-          <span>${name}</span>
-          <sup>${sys.country}</sup>
-        </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-        <figure>
-          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-          <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>
-      `;
-      li.innerHTML = markup;
-      list.append(li);
-    })
-    .catch(() => {
-      msg.textContent = "Este no es un nombre de ciudad valido";
-    })
 
-  msg.textContent = "";
-  list.reset()
-  form.reset();
-  select.focus();
-});
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    //const listItems = list.querySelectorAll(".ajax-section .city");
+    const inputVal = select.value;
+
+    
+    if(navigator.onLine){
+      //AJAX
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const { main, name, sys, weather } = data;
+          const icon = `https://openweathermap.org/img/wn/${
+            weather[0]["icon"]
+          }@2x.png`;
+          //li.classList.add("city");
+          const markup = `
+            <h2 class="city-name" data-name="${name},${sys.country}">
+              <span>${name}</span>
+              <sup>${sys.country}</sup>
+            </h2>
+            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+            <figure>
+              <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+              <figcaption>${weather[0]["description"]}</figcaption>
+            </figure>
+          `;
+          li.innerHTML = markup;
+          //list.append(li);
+        })
+        .catch(() => {
+          msg.textContent = "Este no es un nombre de ciudad valido";
+        })
+
+      msg.textContent = "";
+      form.reset();
+      select.focus();
+
+      // Borra los elementos anteriores y agrega el último resultado
+      list.innerHTML = '';
+      list.appendChild(li);
+      //});
+    }else {
+      alert("No se pueden obtener los datos sin conexión a Internet. Conéctese a la Red e inténtelo de nuevo");
+    }
+  })
